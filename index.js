eb51294617e9349cd098982625f7e9f62e03e076
Chr8s0 <160028141+Chr8s0@users.noreply.github.com>
2026-02-07 22:39:27 +0100

    Haladás

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let elsoKartya = 6
-let masodikKartya = 9
+let elsoKartya = 10
+let masodikKartya = 4
+let osszeg = elsoKartya+masodikKartya
+let blackjack = false
+let elo = true
+let uzenet=''
+let message=document.getElementById('message')
+let sum = document.querySelector('#sum')
 
-let osszeg = elsoKartya + masodikKartya
+let kartya = document.querySelector('#kartya')
 
-console.log(osszeg)
+function startGame(){
+    kartya.textContent = `Kártyák: ${elsoKartya} ${masodikKartya}`
+    sum.textContent = `Összeg: ${osszeg}`
+    if(osszeg <=20){
+        uzenet='Szeretnél kártyát húzni?'
+    } else if(osszeg === 21){
+        uzenet='BlackJack!!!'
+        blackjack=true
+    }else{
+        uzenet='Kiestél!'
+        elo=false
+    }
+    message.textContent=uzenet
+}
